7411d92571dd8aba97eebb2a7982f85bb8385249
vitamin163 <vitalik.domnin@gmail.com>
2020-05-15 01:55:25 +0400

    created 'Hello World'

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const conf = {
+  entry: {
+    app: [
+      './src/index.js',
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '',
+  },
+  devServer: {
+    overlay: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: path.resolve(__dirname, 'node_modules/'),
+        loader: 'babel-loader',
+      },
+      /* {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+          },
+        ],
+      }, */
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
+      /* {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      }, */
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, 'dist/index.html'),
+      template: path.resolve(__dirname, 'src/templates/index.html'),
+    }),
+    /* new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }), */
+  ],
+};
+
+module.exports = (env, options) => {
+  const production = options.mode === 'production';
+  conf.devtool = production ? 'source-map' : 'eval-sourcemap';
+  return conf;
+};

e140a1c0984de5e90b89343cd86237f804635e49
vitamin163 <vitalik.domnin@gmail.com>
2020-05-15 05:57:38 +0400

    created movements

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ function preload() {
 
 let platforms;
 let player;
+let cursors;
 
 function create() {
   this.add.image(400, 300, 'sky');
@@ -53,9 +54,27 @@ function create() {
     repeat: -1,
   });
   this.physics.add.collider(player, platforms);
+  cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+
+    player.anims.play('left', true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+
+    player.anims.play('right', true);
+  } else {
+    player.setVelocityX(0);
+
+    player.anims.play('turn');
+  }
+
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
 }
 
 const config = {

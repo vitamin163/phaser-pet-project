7eea1e1ce99c65b4d6a783f7fee44799efb50f84
vitamin163 <vitalik.domnin@gmail.com>
2020-05-15 05:15:18 +0400

    created ground

--- src/index.js
+++ src/index.js
@@ -17,6 +17,13 @@ function preload() {
 
 function create() {
   this.add.image(400, 300, 'sky');
+  const platforms = this.physics.add.staticGroup();
+
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
 }
 
 function update() {
@@ -26,6 +33,13 @@ const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
   scene: {
     preload,
     create,
